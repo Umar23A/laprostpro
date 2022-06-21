f602f4e8495d9f7f0a6bfd127d0f9486248db154
Umar23A <abduaziz.botirov@mail.ru>
2022-06-21 18:07:39 +0500

    laprost

---
+++ assets/js/sendtelegram.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "5423105428:AAFAWtBJ0dF8_p3U40oCgH-rQg_OlyoaAOs"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
+//chat id
+var chat_id = -1001656459704; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
+var u_name, number, message;
+var ready = function() {
+    u_name = document.getElementById("name").value;
+    number = document.getElementById("number").value;
+    message = document.getElementById("message").value;
+    message = "Ism, Familiya: " + u_name + "\nTelefon raqami " + number + "\nSavol: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("number").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
